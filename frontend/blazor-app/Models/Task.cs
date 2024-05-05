0ece9ab6c32320dd6cdc9764deb99909cedaaff2
war <crypto22h@gmail.com>
2024-05-05 22:04:16 +0100

    create two sortable lists, one for todo and one for done

---
+++ frontend/blazor-app/Models/Task.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorApp.Models;
+
+public class TodoTask
+{
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Status { get; set; }
+}
